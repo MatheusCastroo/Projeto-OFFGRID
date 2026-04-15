e02125ff5ff71b624bddbc0a31ccd93a466a8116
matheuscastro1160@gmail.com <113482506+MatheusCastroo@users.noreply.github.com>
2026-04-15 20:44:37 -0300

    first commit

---
+++ calculo.php
@@ -0,0 +1,47 @@
+<?php
+$equipamentos = [
+    "geladeira" => [
+        "quantidade" => 3,
+        "potencia_w" => 15,
+        "tempo_hora_dia" => 12
+
+    ],
+
+    "televisao" => [
+        "quantidade" => 1,
+        "potencia_w" => 20,
+        "tempo_hora_dia" => 2
+    ],
+    "microondas" => [
+        "quantidade" => 1,
+        "potencia_w" => 65,
+        "tempo_hora_dia" => 2
+    ]
+];
+
+$pontencia_consumida_diaria_1 = $equipamentos["geladeira"]["quantidade"] * $equipamentos["geladeira"]["potencia_w"] * $equipamentos["geladeira"]["tempo_hora_dia"];
+$pontencia_consumida_diaria_2 = $equipamentos["televisao"]["quantidade"] * $equipamentos["televisao"]["potencia_w"] * $equipamentos["televisao"]["tempo_hora_dia"];
+$pontencia_consumida_diaria_3 = $equipamentos["microondas"]["quantidade"] * $equipamentos["microondas"]["potencia_w"] * $equipamentos["microondas"]["tempo_hora_dia"];
+
+$pontencia_consumida_w_1 = $equipamentos["geladeira"]["quantidade"] * $equipamentos["geladeira"]["potencia_w"];
+$potencia_consumida_w_2 = $equipamentos["televisao"]["quantidade"] * $equipamentos["televisao"]["potencia_w"];
+$potencia_consumida_w_3 = $equipamentos["microondas"]["quantidade"] * $equipamentos["microondas"]["potencia_w"];
+
+echo "Geladeira: " . $pontencia_consumida_diaria_1 . "W";
+echo "<br>";
+echo "Televisão: " . $pontencia_consumida_diaria_2 . "W";
+echo "<br>";        
+echo "Microondas: " . $pontencia_consumida_diaria_3 . "W";
+echo "<br>";
+
+$potencia_total_equipamentos = $pontencia_consumida_diaria_1 + $pontencia_consumida_diaria_2 + $pontencia_consumida_diaria_3;
+echo "Potência total consumida pelos equipamentos: " . $potencia_total_equipamentos;
+
+$potencia_total_diaria = ($pontencia_consumida_diaria_1 + $pontencia_consumida_diaria_2 + $pontencia_consumida_diaria_3) / 0.95;
+echo "<br>";        
+echo "Potência total consumida diariamente: " . number_format($potencia_total_diaria,2) . "W";
+
+$potencia_total_equipamentos_w = $pontencia_consumida_w_1 + $potencia_consumida_w_2 + $potencia_consumida_w_3;
+echo "<br>";
+echo "Potência total consumida pelos equipamentos: " . number_format($potencia_total_equipamentos_w,2) . "W";
+?>

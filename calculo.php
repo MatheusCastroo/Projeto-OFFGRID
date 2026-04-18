49bc86a7533f698733d7590f85ebbc4bc71ff92a
matheuscastro1160@gmail.com <113482506+MatheusCastroo@users.noreply.github.com>
2026-04-18 11:19:09 -0300

    Ajustar no calculo

--- calculo.php
+++ calculo.php
@@ -44,4 +44,15 @@ echo "Potência total consumida diariamente: " . number_format($potencia_total_d
 $potencia_total_equipamentos_w = $pontencia_consumida_w_1 + $potencia_consumida_w_2 + $potencia_consumida_w_3;
 echo "<br>";
 echo "Potência total consumida pelos equipamentos: " . number_format($potencia_total_equipamentos_w,2) . "W";
+
+$regioes = [
+ "sul" => 4.2,
+ "norte" => 4.55,
+ "centro-oeste" => 5.25,
+ "sudeste" => 4.55,
+ "nordeste" => 5.6  
+];
+echo "<br>";
+$incidencia_irradiacao_solar = $regioes["sul"];
+echo "Incidência de Irradiação Solar na Região Sul: " . $incidencia_irradiacao_solar . "kWh/m²/dia"; //Puxar de acorodo com a região escolhida
 ?>
